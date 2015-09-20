0f2da7f3cb4e3e7a092848067ca374615c626e2d
Joanna Czarnocka <jczarnocka@onet.pl>
2015-09-20 17:22:56 +0100

    ässigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to quickly access inversed matrix 
+## after inversing it for the first time
 
-## Write a short comment describing this function
+## function caches inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   rev <- NULL
+   set <- function(y) {
+                x <<- y
+                rev <<- NULL
+        }
+   get <- function() x
+   setinv <- function(inversed) rev <<- inversed
+   getinv <- function() rev
+   list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+   
 }
 
 
-## Write a short comment describing this function
+## function takes as argument result of function makeCacheMatrix 
+## checkes whether inversed matrix is placed in cache 
+## and if not inverses it
 
 cacheSolve <- function(x, ...) {
+
         ## Return a matrix that is the inverse of 'x'
+
+	  i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
